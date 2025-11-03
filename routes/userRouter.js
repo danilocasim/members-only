3c170801353ffd8346af1586de2cb731d36cfe51
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-11-03 22:18:01 +0800

    feat: add routing for post request login using local strat and logout route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
-const { addUser } = require("../controller/userController");
-
+const { addUser, logout } = require("../controller/userController");
+const passport = require("passport");
 const userRouter = Router();
 
 userRouter.post("/signup", addUser);
+
+userRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+userRouter.get("/logout", logout);
 
 module.exports = userRouter;
