90c5049c3b06c4928c3634e1491d8410f4f10665
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-11-03 22:20:15 +0800

    feat: add all of the package needed for connecting pg to express-session also for using passport.session()

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const path = require("path");
+const session = require("express-session");
+const pool = require("./db/pool");
+const passport = require("passport");
+
+const pgSession = require("connect-pg-simple")(session);
+
 const indexRouter = require("./routes/indexRouter");
 const userRouter = require("./routes/userRouter");
+const path = require("path");
 const assetsPath = path.join(__dirname, "public");
 
 const PORT = 9000;
@@ -10,7 +16,29 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(assetsPath));
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+require("./configs/passport");
+
+const sessionStore = new pgSession({ pool: pool });
+
+app.use(
+  session({
+    store: sessionStore,
+    secret: "cats",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+  })
+);
+
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/", userRouter);
