52850a811de37bcf9d8323265c39e9f4065c6217
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-11-04 13:39:48 +0800

    feat: add queries for getting all posts, post message, delete post

--- db/queries.js
+++ db/queries.js
@@ -15,3 +15,22 @@ module.exports.addUser = async (
     [firstName, lastName, username, hashedPassword, isAdmin]
   );
 };
+
+module.exports.getAllPosts = async () => {
+  const allPosts = await pool.query(
+    "SELECT posts.id, users.firstname, users.lastname, posts.title, posts.message, posts.timestamp FROM posts INNER JOIN users ON posts.userid = users.id"
+  );
+  return allPosts;
+};
+
+module.exports.postMessage = async (userId, title, message) => {
+  await pool.query(
+    `INSERT INTO posts (userid, title, message, timestamp)
+    VALUES ($1, $2, $3, $4)`,
+    [userId, title, message, new Date()]
+  );
+};
+
+module.exports.deletePost = async (postId) => {
+  await pool.query(`DELETE FROM posts WHERE id = $1`, [postId]);
+};
