6593640d5968f8a988d550e2fba9b8fc4d91b4d7
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-11-05 18:24:22 +0800

    style: adjust structure of errors and make it color red

--- db/dropTables.js
+++ db/dropTables.js
@@ -11,17 +11,34 @@ DROP TABLE IF EXISTS session;
 
 `;
 
-const { DB_NAME, DB_HOST, USERNAME, DB_PASSWORD } = process.env;
+const { NODE_ENV, DB_NAME, DB_HOST, USERNAME, DB_PASSWORD } = process.env;
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGSSLMODE, PGCHANNELBINDING } =
+  process.env;
 
-async function main() {
-  console.log("seeding...");
-  const client = new Client({
-    connectionString: `postgresql://${USERNAME}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`,
-  });
-  await client.connect();
-  await client.query(SQL);
-  await client.end();
-  console.log("done");
+if (NODE_ENV == "dev") {
+  async function main() {
+    console.log("seeding...");
+    const client = new Client({
+      connectionString: `postgresql://${USERNAME}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("done");
+  }
+  main();
 }
 
-main();
+if (NODE_ENV == "prod") {
+  async function main() {
+    console.log("seeding...");
+    const client = new Client({
+      connectionString: `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:5432/${PGDATABASE}`,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("done");
+  }
+  main();
+}
